b7dd4650df799f68d6a5baff81f98370f1263415
rmuell9 <rmatthewmueller@gmail.com>
2025-08-02 17:24:57 -0500

    laptop

---
+++ hypr/scripts/theme.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Check current theme
+current=$(gsettings get org.gnome.desktop.interface gtk-theme)
+echo "Current theme: $current"
+
+if [[ $current == *"dark"* ]]; then
+    # Switch to light
+    gsettings set org.gnome.desktop.interface gtk-theme "Adwaita"
+    gsettings set org.gnome.desktop.interface color-scheme "prefer-light"
+    hyprctl hyprpaper wallpaper "eDP-1,/home/matthew/.config/backgrounds/ren.jpg"
+else
+    # Switch to dark
+    gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
+    gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
+    hyprctl hyprpaper wallpaper "eDP-1,/home/matthew/.config/backgrounds/moonguy.jpg"
+fi
---
+++ shell/env.sh
@@ -0,0 +1,26 @@
+eval "$(/opt/homebrew/bin/brew shellenv)"
+[ -s "$HOME/.config/envman/load.sh" ] && source "$HOME/.config/envman/load.sh"
+eval "$(fzf --zsh)"
+
+function parse_git_branch() {
+    local branch=$(git branch 2> /dev/null | sed -n -e 's/^\* \(.*\)/\1/p')
+    if [[ "$branch" == "(no branch, rebasing master)"* ]]; then
+        echo ""
+    else
+        echo "$branch"
+    fi
+}
+
+COLOR_DEF=$'%f'
+COLOR_GIT=$'%F{39}'
+COLOR_PINK=$'%F{205}'
+setopt PROMPT_SUBST
+export PROMPT='%1~ $(git branch 2>/dev/null >/dev/null && [[ -n "$(parse_git_branch)" ]] && echo "${COLOR_GIT}git:(${COLOR_PINK}%B$(parse_git_branch)%b${COLOR_GIT})${COLOR_DEF} ")'
+
+tmux attach
+clear
+#CTRL-r for fzf CL history
+HISTFILE=~/.zsh_history
+HISTSIZE=10000
+SAVEHIST=10000
+setopt appendhistory
