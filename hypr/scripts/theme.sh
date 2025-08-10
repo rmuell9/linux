3d40bcc735c3ad565dcb58bee426fa3fa5af0a20
rmuell9 <rmatthewmueller@gmail.com>
2025-08-10 01:55:12 -0500

    theme

--- hypr/scripts/theme.sh
+++ hypr/scripts/theme.sh
@@ -6,12 +6,16 @@ echo "Current theme: $current"
 
 if [[ $current == *"dark"* ]]; then
     # Switch to light
+    notify-send "Switched to Light Theme"
     gsettings set org.gnome.desktop.interface gtk-theme "Adwaita"
     gsettings set org.gnome.desktop.interface color-scheme "prefer-light"
     hyprctl hyprpaper wallpaper "eDP-1,/home/matthew/.config/backgrounds/abstract.jpg"
+    pkill hyprsunset
 else
     # Switch to dark
+    notify-send "Switched to Dark Theme"
     gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
     gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
     hyprctl hyprpaper wallpaper "eDP-1,/home/matthew/.config/backgrounds/smoker.jpg"
+    hyprsunset -t 4000
 fi
