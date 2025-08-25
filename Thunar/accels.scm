e087a44b865bfc5faa44033144604d0434036a7c
rmuell9 <rmatthewmueller@gmail.com>
2025-08-25 00:04:47 -0500

    opacity

--- Thunar/accels.scm
+++ Thunar/accels.scm
@@ -67,6 +67,7 @@
 ; (gtk_accel_path "<Actions>/ThunarWindow/zoom-out" "<Primary>minus")
 ; (gtk_accel_path "<Actions>/ThunarWindow/remove-from-recent" "")
 ; (gtk_accel_path "<Actions>/ThunarActionManager/open-with-other" "")
+; (gtk_accel_path "<Actions>/ThunarBookmarks/2277a9dcd2956e7fde546881a0cce6d2" "")
 ; (gtk_accel_path "<Actions>/ThunarStandardView/invert-selection" "<Primary><Shift>i")
 ; (gtk_accel_path "<Actions>/ThunarWindow/view-side-pane-shortcuts" "<Primary>b")
 ; (gtk_accel_path "<Actions>/ThunarWindow/reload-alt-2" "Reload")
